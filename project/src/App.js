4ab1faac4abdcacb6e848fa5a097047cef993b9d
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-08 13:15:31 -0700

    added onClick events to buttons in order to change values in state

--- project/src/App.js
+++ project/src/App.js
@@ -10,11 +10,29 @@ function App() {
   const [balls, setBalls] = useState(0);
   const [fouls, setFouls] = useState(0);
 
+  const resetValues = () => {
+    setStrikes(0);
+    setBalls(0);
+    setFouls(0);
+  }
+
+  const addStrike = () => {
+    strikes === 2 ? resetValues() : setStrikes((strikes + 1));
+  }
+  
+  const addBall = () => {
+    balls === 3 ? resetValues() : setBalls((balls + 1));
+  }
+
+  const addFoul = () => {
+    fouls === 2 ? resetValues() : setFouls((fouls + 1));
+  }
+
 
   return (
     <div className="App">
       <Display strikes={strikes} balls={balls} fouls={fouls} />
-      <Dashboard />
+      <Dashboard addStrike={addStrike} addBall={addBall} addFoul={addFoul} resetValues={resetValues} />
     </div>
   );
 }
